c8a83aa5213d0f89354e591a761c1b325f794e72
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-06-07 17:43:00 +0700

    start

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Trang Đăng nhập</h1>
-      <LoginButton />
-      <Welcome />
-    </div>
+     <> 
+     </>
   );
 }
 
