4a0277da500686ce3f1c72cf2194f6d46feab01e
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-07-07 23:04:24 +0700

    thong tin

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // 🔍 đúng định dạng
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+         
+    },
+  },
+  plugins: [],
+};
